c3d2101e346cf1be2b7c19c25380222dad31a34d
Daniel <ahribori@gmail.com>
2021-03-12 00:37:45 +0900

    tailwind configuration

--- rollup.config.js
+++ rollup.config.js
@@ -44,7 +44,7 @@ export default {
   },
   plugins: [
     svelte({
-      preprocess: sveltePreprocess({ sourceMap: !production }),
+      preprocess: sveltePreprocess({ sourceMap: !production, postcss: true }),
       compilerOptions: {
         // enable run-time checks when not in production
         dev: !production,
